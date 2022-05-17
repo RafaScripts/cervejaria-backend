02a8ddffb0a1bcbdddadf8caf6d2720afdbe0960
Rafael <contato@digitalmoon.tech>
2022-05-17 17:11:44 -0300

    up

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import Routes from "./routes";
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors({ origin: '*', credentials: true }));
 
 app.use(express.json());
 
 app.use(Routes);
 
-app.listen(3333, () => console.log('server is on'));
+app.listen(process.env.PORT || 3333, () => console.log('server is on'));
